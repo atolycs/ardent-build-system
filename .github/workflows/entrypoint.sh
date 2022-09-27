9a7fc3352f09bd1af3d2ab370ecc77858c610cbb
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-27 10:32:45 +0900

    Add Actions

---
+++ .github/workflows/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+if [ -d "/github" ];then
+    sudo chown -R build /github/workspace /github/home
+fi
+WORK_DIR="$(pwd)"
+
+sudo pacman -Sy
+
+export MAKEFLAGS=-j$(nproc)
+ls -1 ${WORK_DIR} | while read line;
+do
+    cd $(WORK_DIR)/${line}
+    namcap PKGBUILD
+    makepkg -fC --syncdeps --noconfirm
+    mv *.pkg.tar.zst ../
+done
+
+echo "================"
+echo "Package created:"
+echo `ls *.pkg.tar.zst`
+echo "================"
