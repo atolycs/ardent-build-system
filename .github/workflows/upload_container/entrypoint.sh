b8c4a758859704455cf25bc45cf4771e0cdb35db
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-27 16:43:24 +0900

    Fix action test

--- .github/workflows/upload_container/entrypoint.sh
+++ .github/workflows/upload_container/entrypoint.sh
@@ -5,14 +5,7 @@ if [ -d "/github" ];then
 fi
 sudo pacman -Sy 
 
-if [ -d "./x86_64" ];then
-    mkdir -p "./x86_64"
-fi
-
 repo-add -k ardent-repo.db.tar.xz *.pkg.tar.zst
-mv *-x86_64.pkg.tar.zst{,.sig} ./ardent-repos/x86_64/
-mv *-any.pkg.tar.zst{,.sig} ./ardent-repos/x86_64/
-mv ardent-repo.* ./ardent-repos/x86_64/
 
 echo "================"
 echo "Package created:"
