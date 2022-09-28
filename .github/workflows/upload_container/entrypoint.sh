f05683420815e5eba2517e6bf25ebc849a5cdc08
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-29 08:43:48 +0900

    Fix Actions Build

--- .github/workflows/upload_container/entrypoint.sh
+++ .github/workflows/upload_container/entrypoint.sh
@@ -3,12 +3,15 @@
 if [ -d "/github" ];then
     sudo chown -R build /github/workspace /github/home
 fi
-sudo pacman -Sy 
 
+echo ${GPG_REPO_SEC} | tee -a ardentlinux_sec.gpg
+
+sudo pacman -Sy 
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 wget https://raw.githubusercontent.com/atolycs/ardentlinux-keyring/master/ardentlinux.gpg
 sudo pacman-key --add ./ardentlinux.gpg
+gpg --import ./ardentlinux_sec.gpg
 gpg --import ./ardentlinux.gpg
 
 ls -lsa
