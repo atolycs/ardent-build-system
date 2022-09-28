7c45421e9ceaf1b4abac818320f1c0ab6294c245
Atolycs <111569596+atolycs@users.noreply.github.com>
2022-09-28 12:40:08 +0900

    Update entrypoint.sh

--- .github/workflows/upload_container/entrypoint.sh
+++ .github/workflows/upload_container/entrypoint.sh
@@ -9,9 +9,14 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 wget https://raw.githubusercontent.com/atolycs/ardentlinux-keyring/master/ardentlinux.gpg
 sudo pacman-key --add ./ardentlinux.gpg
-
+gpg --import ./ardentlinux.gpg
 
 ls -lsa
+
+ls -1 *.pkg.tar.zst | while read line; do
+    gpg --output "${line}.sig" --detach-sig "${line}"
+done
+
 repo-add -n --sign ardent-staging.db.tar.xz *.pkg.tar.zst
 repo-add -n --sign ardent-stable.db.tar.xz *.pkg.tar.zst
 
