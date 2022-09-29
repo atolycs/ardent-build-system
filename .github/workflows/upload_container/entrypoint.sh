08a3a39cf5d2a1f82b497d465647d770c7a5d5e5
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-29 09:44:58 +0900

    Fix Actions Build

--- .github/workflows/upload_container/entrypoint.sh
+++ .github/workflows/upload_container/entrypoint.sh
@@ -5,7 +5,6 @@ if [ -d "/github" ];then
 fi
 
 echo ${GPG_REPO_SEC} | tee -a ardentlinux_sec.gpg
-export GPGKEY=${TEMP_GPG_KEY}
 echo ${TEMP_OWNER_TRUST} | tee -a trust-owner.txt
 
 sudo pacman -Sy 
@@ -16,14 +15,15 @@ sudo pacman-key --add ./ardentlinux.gpg
 gpg --import ./ardentlinux_sec.gpg
 gpg --import ./ardentlinux.gpg
 gpg --import-ownertrust ./trust-owner.txt
+export GPGKEY=${TEMP_GPG_KEY}
 ls -lsa
 
 ls -1 *.pkg.tar.zst | while read line; do
     gpg --output "${line}.sig" --detach-sig "${line}"
 done
 
-repo-add -n --sign ardent-staging.db.tar.xz *.pkg.tar.zst
-repo-add -n --sign ardent-stable.db.tar.xz *.pkg.tar.zst
+repo-add -n --sign -k ${TEMP_GPG_KEY} ardent-staging.db.tar.xz *.pkg.tar.zst
+repo-add -n --sign -k ${TEMP_GPG_KEY} ardent-stable.db.tar.xz *.pkg.tar.zst
 
 
 echo "================"
