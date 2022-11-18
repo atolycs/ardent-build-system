e0efdba6afec25690c328063d416c8357413201a
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-11-18 10:57:41 +0900

    [actions] Keys Update Test

--- .github/workflows/upload_container/entrypoint.sh
+++ .github/workflows/upload_container/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -d "/github" ];then
     sudo chown -R build /github/workspace /github/home
 fi
 
-export GPG_TTY=$(tty)
+#export GPG_TTY=$(tty)
 #echo "${ATOLYCS_PASSPHRASE}" | base64 --decode
 echo -n "${PACK_PRIVATE_PGP_KEY}" | base64 --decode | gpg --batch --import --passphrase "$(echo ${ATOLYCS_PASSPHRASE} | base64 --decode)"
 #gpg --import keys.gpg --passphrase $(cat atolycs_pass.txt)
